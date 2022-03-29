ed11502d1ebcebf12e4482423d28c5ce6d88f1aa
Pedro Borges <pedroborges@MacBook-Air-de-Pedro.local>
2022-03-29 00:53:00 -0300

    Initial Commit

---
+++ backend/src/resolvers/UserResolver.ts
@@ -0,0 +1,30 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from './../models/User';
+import Crypto from 'crypto'
+
+
+
+@Resolver()
+export class UserResolver {
+
+  private data: User[] = [];
+
+  @Query(() => [User])
+  async users() {
+    return this.data
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg('name') name: string
+  ) {
+    const user = {
+      id: Crypto.randomUUID(),
+      name
+    }
+
+    this.data.push(user)
+
+    return user;
+  }
+}
